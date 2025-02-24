541e3acf68ef63540b65e4c48997aed52fe8df59
Sharanya-Murlidharan <sharanyamurlidharan8@gmail.com>
2025-02-24 16:43:33 +0530

    Time: 0 ms (100%), Space: 53.7 MB (7.43%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+     let len = stones.length;
+    for (let i = 0; i < jewels.length; i++) stones = stones.replaceAll(jewels[i], "");
+    return len - stones.length;
+};
