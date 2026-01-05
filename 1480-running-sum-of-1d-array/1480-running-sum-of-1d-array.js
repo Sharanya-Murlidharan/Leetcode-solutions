4c504585537b9cb093eb9188a0ff592759394355
Sharanya-Murlidharan <sharanyamurlidharan8@gmail.com>
2026-01-05 19:24:00 +0530

    Time: 0 ms (100%), Space: 55.3 MB (76.79%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let output = new Array(nums.length)
+    output[0] = nums[0];
+    for(let idx = 1; idx < nums.length; idx++) {
+        output[idx] = output[idx-1] + nums[idx];
+    }
+    return output;
+};
