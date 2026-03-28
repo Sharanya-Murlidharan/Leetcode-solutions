99cc823010e1bf85b9fcd29670e1acbe8a68ecfd
Sharanya-Murlidharan <sharanyamurlidharan8@gmail.com>
2026-03-28 12:06:12 +0530

    Time: 1 ms (58.12%), Space: 56.6 MB (28.46%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let ans=0;
+   for(let i=0;i<s.length-1;i++){
+    ans+=Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1));
+   }
+   return ans;
+};
